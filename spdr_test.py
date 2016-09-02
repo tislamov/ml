0ba1cfeaf9689d805b4fdc05a1c3ad61f762fa09
Timur Islamov <timur@ospreydata.com>
2016-09-02 10:33:55 -0700

    initial commit

--- spdr_test.py
+++ spdr_test.py
@@ -6,16 +6,24 @@ from pyspark import SparkContext
 
 pd.set_option("display.width", 10000)
 
+ohlc_dict = {
+    'o': 'first',
+    'h': 'max',
+    'l': 'min',
+    'c': 'last',
+    'v': 'sum',
+}
+
 
 def check_combo(combo):
-
-    for g in d.groupby(combo):
-        if g[1].shape[0] >= 500:
+    for g in train.groupby(combo):
+        if g[1].shape[0] >= 100:
             for symbol in symbols:
                 bc = np.bincount(g[1][symbol])
                 wr = float(bc[1]) / g[1].shape[0]
-                if wr > 0.49:
-                    r = {"c": combo, "p": g[0], "s": symbol, "wr": wr, "len": g[1].shape[0]}
+                cp = (g[1]["{}_pct".format(symbol)] * 1 + 1).cumprod()[-1]
+                if cp > 1.05:
+                    r = {"c": combo, "p": g[0], "s": symbol, "wr": wr, "len": g[1].shape[0], "cp": cp}
                     print r
                     yield r
 
@@ -29,6 +37,7 @@ symbols = filter(lambda s: s not in ["XLFS", "XLRE"], symbols)
 temp = []
 for symbol in symbols:
     d = store[symbol]
+    d = d.resample("5T", how=ohlc_dict)
     print symbol, d.shape[0], d["v"].sum()
     temp.append(d[["c"]])
 
@@ -51,19 +60,29 @@ d = d.sort_index(1)
 
 d = d.dropna()
 
+# train = d.iloc[:400]
+# test = d.iloc[400:]
+
+train = d
+test = d
+
+print train.shape, test.shape
+
 # print d[symbols]
 # print d[offset_symbols]
 
-# dd = d[~d["XLE_2"] & ~d["XLK_1"] & d["XLY_1"]]
-
-# print (dd["XLK_pct"] + 1).cumprod()[-1]
+# train_sel = train[(train["XLF_4"] == False) & (train["XLY_1"] == False)]
+# print train.shape[0], train_sel.shape[0], (train_sel["XLE_pct"] + 1).cumprod()[-1]
+# test_sel = test[(test["XLF_4"] == False) & (test["XLY_1"] == False)]
+# print test.shape[0], test_sel.shape[0], (test_sel["XLE_pct"] + 1).cumprod()[-1]
+#
 # raise
 
 for symbol in symbols:
     print symbol, np.bincount(d[symbol])
 
 combos = []
-for n in xrange(3, 6):
+for n in xrange(2, 5):
     for c in itertools.combinations(offset_symbols, n):
         combos.append(c)
 
@@ -76,5 +95,4 @@ result = combos_RDD.flatMap(check_combo).collect()
 sc.stop()
 
 result = pd.DataFrame(result)
-print result.sort_values("wr")
-
+print result.sort_values("cp")
